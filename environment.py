359754b25a6d7bc81995c879eb58e741055b2bbb
Benedikt Schlegel <47198081+BenediktJSchlegel@users.noreply.github.com>
2023-09-20 21:53:32 +0200

    Update environment.py

--- environment.py
+++ environment.py
@@ -2,10 +2,22 @@ import sys
 import urllib.request
 
 class EnvironmentHelper:
-  def __init__(self, local, colab):
+  def __init__(self, local_data_root, colab_data_root, colab_root, local_root):
     self.is_colab = 'google.colab' in sys.modules
-    self.colab_base = colab
-    self.local_base = local
+    self.colab_base = colab_data_root
+    self.local_base = local_data_root
+    self.colab_workdir = colab_root
+    self.local_workdir = local_root
+
+    self.include_scripts()
+
+
+  def include_scripts(self):
+    if self.is_colab:
+      sys.path.append(self.colab_workdir + '/content/functions')
+    else:
+      sys.path.append(self.local_workdir + '/content/functions')
+
 
   def get_path(self):
     if self.is_colab:
